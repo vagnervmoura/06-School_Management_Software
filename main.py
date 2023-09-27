05e24c61af4870a535f64896b743b11e9794377e
Vagner Vignoto Moura <eu@vagnermoura.com>
2023-09-27 21:17:40 +0100

    Added function Homeroom and visual edition on menu

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
 # School Management Software
 
+# on terminal to execute this EMPLOYEE CLASSES and get the inputs from a text file "employees.txt"
+# type on terminal: Get-Content input_school.txt | python main_test_school.py 
+
 v_option = None
 list_of_students = []
 list_of_teachers = []
+list_of_homeroom_teachers = []
+v_name = None
+v_classe = None
+number = 0
+classe_of_students = []
+
 
 def goto(linenum):
     global line
@@ -24,6 +33,13 @@ class Teacher:
         self.subject = subject
         self.classe = classe
 
+class HomeroomTeacher(Teacher):
+    def __init__(self, name, classe, students):
+        super().__init__(name, classe, students)
+        self.students = students
+        
+    def describe(self):
+        print(f"{self.name} teaches in {self.classe} and has {self.students} students")
 
         
 #print(list_of_students)
@@ -49,7 +65,9 @@ def f_create():
                     classe = input("Enter the class of the student: ")
         
                     new_student = Student(name=name, classe=classe)
-                    list_of_students.append(new_student)
+                    student_name = new_student.name
+                    student_classe = new_student.classe
+                    list_of_students.append({"v_name": student_name, "v_classe": student_classe})
                     print(f"student {name} added")
         
         elif v_create == 2:
@@ -66,6 +84,31 @@ def f_create():
                     new_teacher = Teacher(name=name, subject=subject, classe=classe)
                     list_of_teachers.append(new_teacher)
                     print(f"teacher {name} added")
+                    
+        elif v_create == 3:
+            print("You choose the option {} to create a new Homeroom Teacher.".format(v_create))
+            while True:
+                selection = input("Enter 'a' to add a new Homeroom Teacher or press 'q'to quit to main menu: ")
+                if selection == "q":
+                    break
+                elif selection == "a":
+                    name = input("Enter the name of the Homeroom Teacher: ")
+                    classe = input("Enter the class of the Homeroom Teacher: ")
+                    for students_in_classe in list_of_students:
+                        if students_in_classe["v_classe"] == classe:
+                            classe_of_students.append(students_in_classe["v_name"])
+                    print()                          
+                    
+                    new_homeroom_teacher = HomeroomTeacher(name=name, classe=classe, students=classe_of_students)
+                    list_of_homeroom_teachers.append(new_homeroom_teacher)
+                    number_of_students = len(list_of_homeroom_teachers) + 1
+                    print(f"Homeroom Teacher {name} added")
+                    print(f"Homeroom Teacher {name} has {len(new_homeroom_teacher.students)} students.")
+                    print(f"List of students in class: ")
+                    print(*new_homeroom_teacher.students, sep = "\n")
+                    print()
+                    print()
+ 
         
     except ValueError:
         print("Sorry, you did not input a valid value.\n")          
@@ -78,7 +121,17 @@ def f_manage():
 # MAIN MENU
 line = 1
 while v_option != 0:
-    v_option = int(input("\n\nSelect one of the following options:\n1 - Create\n2 - Manage\n0 - end\n\n"))
+    print()
+    print(" "*6+"/" * 50 + "\n" +" "*6+ "/" + " " * 12 + "School Management System" + " " * 12 + "/" + "\n" +" "*6+ "/" * 50)
+    print(" "*6+"/" + " " * 48 + "/", sep="")
+    print(" "*6+"/"+" "*6+"Select one of the following options:"+" "*6+"/")
+    print(" "*6+"/"+" "*6+"="*36+" "*6+"/")
+    print(" "*6+"/"+" "*15+"1 - Create"+" "*23+"/")
+    print(" "*6+"/"+" "*15+"2 - Manage"+" "*23+"/")
+    print(" "*6+"/"+" "*15+"0 - end"+" "*26+"/")
+    v_option = int(input())
+    print(" "*6+"/" + " " * 48 + "/", sep="")
+    print(" "*6+"/" * 50)
 
     try:
         if v_option == 0:
@@ -86,11 +139,11 @@ while v_option != 0:
             exit()
             
         elif v_option == 1: # option to CREATE
-            print("You choose the v_option {} to Create.".format(v_option))
+            print("You choose the option {} to Create.".format(v_option))
             f_create()
             
         elif v_option == 2: # option to MANAGE
-            print("You choose the v_option {} to Manage.".format(v_option))
+            print("You choose the option {} to Manage.".format(v_option))
             f_manage()
     
     except ValueError:
